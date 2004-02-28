c6d94f2176b7f78fe06b5811c7dc161e4001ea29
edwards <edwards>
2004-02-28 21:58:50 +0000

    Removed PARALLEL_READ/WRITE flags.

--- include/config.h
+++ include/config.h
@@ -3,15 +3,11 @@
 
 #include "qio_config.h"
 
-/* THIS NEEDS TO BE AUTOCONFIG'ED */
+/* DO NOT AUTOCONF THIS STUFF - WILL BE REMOVED */
 
 /* Are 32 bit integers and floats stored in big endian on this
    architecture?  Our files are always big endian. */
 #define QIO_BIG_ENDIAN 0
-
-/* Does the architecture support parallel reads and writes? */
-#define PARALLEL_READ 0
-#define PARALLEL_WRITE 0
 
 /* For BinX record */
 #undef DO_BINX
