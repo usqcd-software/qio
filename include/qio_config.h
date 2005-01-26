16eff3e62f48ce9a919ebbb8c2647d5da38dc9c0
zbigniew <zbigniew>
2005-01-26 14:21:17 +0000

    Changed the redefinitions of the macros generated by autoconf. Previously, the macro redefinitions didn't work, so now this info is stored in C-strings.

--- include/qio_config.h
+++ include/qio_config.h
@@ -15,13 +15,13 @@
 #include "qio_config_internal.h"
 
 /* Prefix everything with QIO_ */
-#define QIO_PACKAGE PACKAGE
-#define QIO_PACKAGE_BUGREPORT PACKAGE_BUGREPORT
-#define QIO_PACKAGE_NAME PACKAGE_NAME
-#define QIO_PACKAGE_STRING PACKAGE_STRING
-#define QIO_PACKAGE_TARNAME PACKAGE_TARNAME
-#define QIO_PACKAGE_VERSION PACKAGE_VERSION
-#define QIO_VARSION VERSION
+static const char* const QIO_PACKAGE = PACKAGE;
+static const char* const QIO_PACKAGE_BUGREPORT = PACKAGE_BUGREPORT;
+static const char* const QIO_PACKAGE_NAME = PACKAGE_NAME;
+static const char* const QIO_PACKAGE_STRING = PACKAGE_STRING;
+static const char* const QIO_PACKAGE_TARNAME = PACKAGE_TARNAME;
+static const char* const QIO_PACKAGE_VERSION = PACKAGE_VERSION;
+static const char* const QIO_VERSION = VERSION;
 
 
 /* Undef the unwanted */
